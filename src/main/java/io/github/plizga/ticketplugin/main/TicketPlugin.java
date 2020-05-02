77957f149f9ed1b299f7129b6123acc9e27cbda9
JB Plizga <bradplizga@gmail.com>
2020-05-02 02:08:06 -0700

    Initial commit

---
+++ src/main/java/io/github/plizga/ticketplugin/main/TicketPlugin.java
@@ -0,0 +1,20 @@
+package io.github.plizga.ticketplugin.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TicketPlugin extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
